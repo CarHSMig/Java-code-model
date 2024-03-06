d369d29edc03bd8ba31fc5b942c5de367e62efba
CarHSMig <151793880+CarHSMig@users.noreply.github.com>
2024-03-05 21:53:52 -0300

    Create Atividade3.1.java

---
+++ Atividade3.1.java
@@ -0,0 +1,21 @@
+    import java.util.Scanner;
+
+   class calculo{
+        public static void main(String[] args) {
+
+            Scanner geral = new Scanner(System.in);
+
+            System.out.println("Informe o valor 1:");
+            float valor1 = geral.nextFloat();
+
+            System.out.println("Informe o valor 2:");
+            float valor2 = geral.nextFloat();
+
+            System.out.println("Informe o valor 1:");
+            float valor3 = geral.nextFloat();
+
+
+            float valorTotal = valor1 + valor2 + valor3;
+            System.out.printf("O Valor total é:%s", valorTotal);
+        }
+           }
